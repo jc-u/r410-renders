5589493bf8f201dd5ba9b20cb53a469e95587cc7
Jean-Christophe Urbain <jcurbain.admin@gmail.com>
2023-02-10 19:45:26 +0100

    Optimisation des rendus de Notification

--- r410-introduction/src/App.jsx
+++ r410-introduction/src/App.jsx
@@ -1,24 +1,34 @@
-import React from 'react';
-import Cards from './Cards';
+import React, { useState } from "react";
+import Cards from "./Cards";
+import NotificationsCenter from "./NotificationCenter";
 import cardsData from "./constants/cardsData";
 import useNotificationsData from "./hooks/useNotificationsData";
-import NotificationsCenter from './NotificationCenter';
 import useRendersNumber from "./hooks/useRendersNumbers";
 
-
 function App() {
-  const { notificationsData, addNotification, deleteNotification } = useNotificationsData();
+  const { notificationsData, addNotification, deleteNotification } =
+    useNotificationsData();
   const rendersNumber = useRendersNumber();
+  const [dummy, setDummy] = useState(0);
 
   return (
     <div className="app">
       <header className="app__header header">
-        <h1 className="header__title">[{rendersNumber}] Introduction to React</h1>
+        <h1 className="header__title">
+          [{rendersNumber}] Introduction to React
+        </h1>
       </header>
-      <main className="app__main"> 
-      <button className="btn" type="button" onClick={addNotification}>
+      <main className="app__main">
+        <button className="btn" type="button" onClick={addNotification}>
           Add a notification
-      </button>
+        </button>
+        <button
+          type="button"
+          className="btn"
+          onClick={() => setDummy(dummy + 1)}
+        >
+          {dummy}
+        </button>
         <Cards cardsData={cardsData} />
       </main>
       <footer className="app__footer footer">
